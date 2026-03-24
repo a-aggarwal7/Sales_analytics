7517b928b807ad83aad165b65d6b63244527bc94
a-aggarwal7 <113556536+a-aggarwal7@users.noreply.github.com>
2026-03-24 14:58:45 -0600

    Added the sql script

---
+++ sql/sales_cleaning.sql
@@ -0,0 +1,117 @@
+CREATE DATABASE sales_analytics_project;
+
+USE sales_analytics_project;
+
+SELECT * 
+FROM new_sales_data;
+
+-- CREATING A DUPLICATE STAGING TABLE SO THE REAL DATA REMAINS UNAFFECTED
+CREATE TABLE sales_staging
+LIKE new_sales_data;
+
+INSERT sales_staging
+SELECT *
+FROM new_sales_data;
+
+SELECT COUNT(*) 
+FROM sales_staging;
+
+DESCRIBE sales_staging;
+
+-- Never executed
+UPDATE sales_staging 
+SET 
+  `Order Date` = STR_TO_DATE(`Order Date`, '%m/%d/%Y'),
+  `Ship Date` = STR_TO_DATE(`Ship Date`, '%m/%d/%Y');
+
+ALTER TABLE sales_staging
+  MODIFY COLUMN `Order Date` DATE,
+  MODIFY COLUMN `Ship Date` DATE,
+  MODIFY COLUMN `Postal Code` INT,
+  MODIFY COLUMN Sales DECIMAL(10,2),
+  MODIFY COLUMN Quantity INT,
+  MODIFY COLUMN Discount DECIMAL(5,2),
+  MODIFY COLUMN Profit DECIMAL(10,2);
+-- Till here 
+
+
+
+
+
+-- 1. REMOVE DUPLICATES
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY `Order ID`, `Product ID`, `Customer ID`, `Order Date`, `Ship Date`, `Customer Name`, `Segment`, Country, City, State, `Postal Code`, Region, Category, `Product Name`, Sales, Quantity, Discount, Profit) AS row_num 
+FROM sales_staging;
+
+
+WITH duplicate_cte AS 
+(
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY `Order ID`, `Product ID`, `Customer ID`, `Order Date`, `Ship Date`, `Customer Name`, `Segment`, Country, City, State, `Postal Code`, Region, Category, `Product Name`, Sales, Quantity, Discount, Profit) AS row_num 
+FROM sales_staging
+)
+SELECT * 
+FROM duplicate_cte
+WHERE row_num > 1;
+
+-- double checking the duplicates
+SELECT * 
+FROM sales_staging
+WHERE `Order ID` = 'US-2014-150119';
+
+-- found the duplicate record, will now delete the duplicate record
+
+CREATE TABLE sales_staging2
+LIKE sales_staging;
+
+ALTER TABLE sales_staging2
+ADD row_num int; 
+
+INSERT sales_staging2
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY `Order ID`, `Product ID`, `Customer ID`, `Order Date`, `Ship Date`, `Customer Name`, `Segment`, Country, City, State, `Postal Code`, Region, Category, `Product Name`, Sales, Quantity, Discount, Profit) AS row_num 
+FROM sales_staging;
+
+SELECT * 
+FROM sales_staging2;
+
+SELECT * 
+FROM sales_staging2
+WHERE row_num > 1;
+
+DELETE 
+FROM sales_staging2
+WHERE row_num > 1;
+
+
+
+
+-- 2. Standardize the data 
+SELECT * 
+FROM sales_staging2;
+
+-- Handling white spaces with trim function
+SELECT DISTINCT(`Sub-Category`)
+FROM sales_staging2;
+
+UPDATE sales_staging2
+SET State = TRIM(State);
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- 3. Handling null or blank values
+
+-- 4. Remove any columns
